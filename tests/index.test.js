f85c492f967379367642c4118ca64922d340e17f
MADHUKAR SAMMETA <155879431+iammadhukar177@users.noreply.github.com>
2025-09-10 16:46:54 +0530

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,4 @@
-const { main } = require('../index');
+const { main } = require('src/index');   // <--- destructure the function
 
 test('should perform the primary task correctly', () => {
     expect(main()).toBe("Hello from GitHub Actions!");
